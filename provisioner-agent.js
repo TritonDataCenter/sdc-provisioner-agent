f36c9d4f2ba13410c8ff0de04a226ed6abc338ab
Orlando Vazquez <ovazquez@gmail.com>
2010-07-18 08:05:13 -0700

    Initial commit of provisioner agent

--- provisioner-agent.js
+++ provisioner-agent.js
@@ -1,39 +1,13 @@
-exec = require('child_process').exec;
-fs = require("fs");
-sqlite = require("./node-sqlite/sqlite");
-sys  = require('sys');
-path  = require('path');
-zonezfs = require('./lib/zonezfs');
+require.paths.unshift('./lib');
+require.paths.unshift('.');
 
-AMQPAgent = require('./amqp_agent').AMQPAgent;
-
-function CollectorAgent(config) {
-  var self = this;
-
-  config = config || {};
-  config.amqp = config.amqp || {};
-  config.resource = "provisioner";
-  config.database_path = config.database_path
-                         || path.join(__dirname, "collector.db");
-  self.debug = config.debug;
-
-  AMQPAgent.call(this, config);
-
-  this.registerCommand("provision");
-}
-
-sys.inherits(CollectorAgent, AMQPAgent);
-
-CollectorAgent.prototype.onCommand = function (command, msg) {
-  var self = this;
-  if (command !== "query")
-    return;
-
-  self.ackSuccess(msg.id);
-}
+ProvisionerAgent = require('provisioner').ProvisionerAgent;
 
 function main() {
-  var agent = new ProvisionerAgent();
+  var config = {
+    hostname: 'sagan'
+  };
+  var agent = new ProvisionerAgent(config);
   agent.connect(function () {
     puts("Ready to rock.");
   });
---
+++ tests/test-provisioner-agent.js
@@ -0,0 +1,92 @@
+require.paths.push(__dirname + '/..');
+require.paths.push(__dirname + '/../lib');
+
+sys = require('sys');
+exec = require('child_process').exec;
+zfs = require('zfs').zfs;
+fs = require('fs');
+path = require('path');
+
+puts = sys.puts;
+inspect = sys.inspect;
+
+ProvisionerAgent = require('provisioner').ProvisionerAgent;
+ProvisionerClient = require('amqp_agent/client').Client;
+
+TestSuite = require('async-testing/async_testing').TestSuite;
+
+var suite = exports.suite = new TestSuite("Provisioner Agent Tests");
+var hostname;
+
+var tests = [
+  { 'Test sending a provision command':
+    function (assert, finished) {
+      var msg = { zone_name: 'orlandozone' };
+
+      this.agent.sendCommand('provision', msg,
+        function (reply) {
+          assert.equal(reply.error, undefined, "Error should be unset");
+          finished();
+        });
+    }
+  }
+];
+
+// order matters in our tests
+for (i in tests) {
+  suite.addTests(tests[i]);
+}
+
+var client;
+var agent;
+
+function startAgent(callback) {
+  var config = {
+    hostname: 'sagan'
+  };
+  agent = new ProvisionerAgent(config);
+  agent.connect(function () {
+    puts("Ready to rock.");
+    callback && callback();
+  });
+}
+
+suite.setup(function(finished, test) {
+  var self = this;
+  if (client) {
+    self.agent = client.getAgentHandle(hostname, 'provisioner');
+    finished();
+  }
+  else {
+    exec('hostname', function (err, stdout, stderr) {
+      hostname = stdout.trim();
+      var dot = hostname.indexOf('.');
+      if (dot !== -1) hostname = hostname.slice(0, dot);
+
+      startAgent(function () {
+        config = { reconnect: false };
+        client = new ProvisionerClient(config);
+        client.connect(function () {
+          self.agent = client.getAgentHandle(hostname, 'provisioner');
+          finished();
+        });
+      });
+    });
+  }
+})
+
+var currentTest = 0;
+var testCount = tests.length;
+
+suite.teardown(function() {
+  if (++currentTest == testCount) {
+    process.nextTick(function () {
+      agent.end();
+      client.end();
+    });
+  }
+});
+
+if (module == require.main) {
+  suite.runTests();
+}
