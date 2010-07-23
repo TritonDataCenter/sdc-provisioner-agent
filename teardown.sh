bda65012879a39d7eae22f811a2a869ee14ec0df
Orlando Vazquez <ovazquez@gmail.com>
2010-07-22 18:36:40 -0700

    Update amqp_agent repo

--- teardown.sh
+++ teardown.sh
@@ -3,18 +3,18 @@ echo "Cleaning up zone $ZONENAME"
 ZONENAME=orlandozone
 
 # deactivate
-sudo /usr/sbin/zoneadm -z $ZONENAME halt
-sudo /usr/sbin/zonecfg -z $ZONENAME set autoboot=false
+/usr/sbin/zoneadm -z $ZONENAME halt
+/usr/sbin/zonecfg -z $ZONENAME set autoboot=false
 
 # destroy
-sudo /usr/sbin/zoneadm -z $ZONENAME uninstall -F
-sudo /usr/sbin/zonecfg -z $ZONENAME delete -F
+/usr/sbin/zoneadm -z $ZONENAME uninstall -F
+/usr/sbin/zonecfg -z $ZONENAME delete -F
 
-sudo zfs destroy -f -R zones/nodejs@$ZONENAME
-sudo zfs destroy -f zones/$ZONENAME
-sudo rm /etc/zones/$ZONENAME.xml
-sudo cp /etc/zones/index-orlando /etc/zones/index
+zfs destroy -f -R zones/nodejs@$ZONENAME
+zfs destroy -f zones/$ZONENAME
+rm /etc/zones/$ZONENAME.xml
+cp /etc/zones/index-orlando /etc/zones/index
 
 # destroy vnic
-sudo /usr/sbin/dladm delete-vnic ${ZONENAME}0
-sudo /usr/sbin/dladm delete-vnic ${ZONENAME}2
+/usr/sbin/dladm delete-vnic ${ZONENAME}0
+/usr/sbin/dladm delete-vnic ${ZONENAME}2
