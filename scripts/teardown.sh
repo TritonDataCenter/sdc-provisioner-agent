b83026e9723d1f3861614407a158fdaa7282c1bc
Orlando Vazquez <ovazquez@gmail.com>
2010-07-23 20:18:18 -0700

    Move scripts to scripts/ directory

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -10,11 +10,6 @@ ZONENAME=orlandozone
 /usr/sbin/zoneadm -z $ZONENAME uninstall -F
 /usr/sbin/zonecfg -z $ZONENAME delete -F
 
-zfs destroy -f -R zones/nodejs@$ZONENAME
-zfs destroy -f zones/$ZONENAME
-rm /etc/zones/$ZONENAME.xml
-cp /etc/zones/index-orlando /etc/zones/index
-
 # destroy vnic
-/usr/sbin/dladm delete-vnic ${ZONENAME}0
-/usr/sbin/dladm delete-vnic ${ZONENAME}2
+/usr/sbin/dladm delete-vnic ${ZONENAME}0 || true
+/usr/sbin/dladm delete-vnic ${ZONENAME}2 || true
