a2a8b24f9ef477f98aeabb1896ca4958d78800fc
Orlando Vazquez <ovazquez@gmail.com>
2010-07-23 20:19:40 -0700

    Update bamboo file for provisioner

--- bamboo.sh
+++ bamboo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Script to build collector in a bamboo remote running on a global zone,
+# Script to build provisioner in a bamboo remote running on a global zone,
 # install it, and run some tests.
 
 checkout() {
@@ -10,35 +10,27 @@ checkout() {
 }
 
 build() {
-#   gmake distclean
-#   gmake all
-  pushd amqp_agent/mDNS
-  node-waf configure build
-  popd
+  gmake distclean
+  gmake all
 }
 
 teardown() {
-  if pkginfo -q JOYcollector; then
-    sudo pkgrm -a ./admin -n JOYcollector
+  if pkginfo -q JOYprovisioner; then
+    sudo pkgrm -a ./admin -n JOYprovisioner
   fi
-  if svcs -v collector 2> /dev/null; then
-    echo "ERROR: collector service still running after teardown!"
+  if svcs -v provisioner 2> /dev/null; then
+    echo "ERROR: provisioner service still running after teardown!"
     exit 1
   fi
-  sudo rm -f /var/db/collector/collector.db
 }
 
 install() {
-  sudo pkgadd -a ./admin -G -d ./JOYcollector-*.pkg all
+  sudo pkgadd -a ./admin -G -d ./JOYprovisioner-*.pkg all
 
-  cat << __EOF__ | sudo sh -c 'cat - > /opt/collector/etc/collector.ini'
+  cat << __EOF__ | sudo sh -c 'cat - > /opt/provisioner/etc/provisioner.ini'
 ; Look up AMQP broker host via mDNS. Otherwise, specify a "host" and "port"
 ; parameter in the "amqp" section.
 ; mdns = amqp-broker
-database_path = /var/db/collector/collector.db
-
-; Ignore these zones
-ignore_zone_ids = 0
 
 ; Hostname. If unset will use output of "hostname" command.
 ;hostname = foo
@@ -49,16 +41,16 @@ login = joyent
 password = joytastic
 __EOF__
 
-  sudo svcadm restart collector
+  sudo svcadm restart provisioner
 
-  if ! svcs -v collector 2> /dev/null; then
-    echo "ERROR: collector service not running after install!"
+  if ! svcs -v provisioner 2> /dev/null; then
+    echo "ERROR: provisioner service not running after install!"
     exit 1
   fi
 
-  COLLECTOR_LOG=/var/svc/log/site-collector\:default.log
-  echo "Last 100 lines of $COLLECTOR_LOG"
-  tail -n 100 $COLLECTOR_LOG
+  PROVISIONER_LOG=/var/svc/log/site-provisioner\:default.log
+  echo "Last 100 lines of $PROVISIONER_LOG"
+  tail -n 100 $PROVISIONER_LOG
 }
 
 run_test() {
@@ -74,7 +66,7 @@ run_test() {
 
 checkout
 build
-#teardown
-#install
+teardown
+install
 
 run_test
