649758e92f1d8f03b55ed70bd0d0de4e9c1cedfe
Bamboo remote agent <bamboo@angel.joyent.us>
2010-07-29 04:29:16 +0000

    Make sure zfs datasets are deleted

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -10,6 +10,9 @@ echo "Cleaning up zone $ZONENAME"
 /usr/sbin/zoneadm -z $ZONENAME uninstall -F
 /usr/sbin/zonecfg -z $ZONENAME delete -F
 
+/usr/sbin/zfs destroy zones/ZONENAME
+/usr/sbin/zfs destroy zones/nodejs@$ZONENAME
+
 # destroy vnic
 /usr/sbin/dladm delete-vnic ${ZONENAME}0 || true
 /usr/sbin/dladm delete-vnic ${ZONENAME}2 || true
