13aa1d9b8c891bb4e4e116cbb07b3e19cbc8e2d5
Bamboo remote agent <bamboo@angel.joyent.us>
2010-07-29 05:24:58 +0000

    Robustify teardown script

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -2,17 +2,23 @@ echo "Cleaning up zone $ZONENAME"
 
 # ZONENAME=orlandozone
 
+if [ -z "$ZONENAME" ];
+then
+  echo "ZONENAME environment variable must be set" >&2
+  exit 1
+fi
+
 # deactivate
-/usr/sbin/zoneadm -z $ZONENAME halt
-/usr/sbin/zonecfg -z $ZONENAME set autoboot=false
+/usr/sbin/zoneadm -z "$ZONENAME" halt
+/usr/sbin/zonecfg -z "$ZONENAME" set autoboot=false
 
 # destroy
-/usr/sbin/zoneadm -z $ZONENAME uninstall -F
-/usr/sbin/zonecfg -z $ZONENAME delete -F
+/usr/sbin/zoneadm -z "$ZONENAME" uninstall -F
+/usr/sbin/zonecfg -z "$ZONENAME" delete -F
 
-/usr/sbin/zfs destroy zones/ZONENAME
-/usr/sbin/zfs destroy zones/nodejs@$ZONENAME
+/usr/sbin/zfs destroy -f "zones/$ZONENAME"
+/usr/sbin/zfs destroy -f "zones/nodejs@$ZONENAME"
 
 # destroy vnic
-/usr/sbin/dladm delete-vnic ${ZONENAME}0 || true
-/usr/sbin/dladm delete-vnic ${ZONENAME}2 || true
+/usr/sbin/dladm delete-vnic "${ZONENAME}0" || true
+/usr/sbin/dladm delete-vnic "${ZONENAME}2" || true
