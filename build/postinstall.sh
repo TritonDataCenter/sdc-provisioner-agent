7166f46549d36b8e20f3373207cd74c8dbe9c5bf
Orlando Vazquez <ovazquez@gmail.com>
2010-09-22 17:38:03 -0700

    The provisioner shouldn't delete existing .ini file on pkgrm. Fixes NODE-63

--- build/postinstall.sh
+++ build/postinstall.sh
@@ -3,6 +3,10 @@ svccfg import $BASEDIR/provisioner/provisioner.xml
 svcadm enable provisioner
 svcs -v provisioner
 
+if [ ! -f $BASEDIR/provisioner/etc/provisioner.ini ]; then
+  cp $BASEDIR/provisioner/etc/provisioner.ini-sample $BASEDIR/provisioner/etc/provisioner.ini
+fi
+
 echo ""
 echo "Edit $BASEDIR/provisioner/etc/provisioner.ini to set you AMQP config"
 echo ""
