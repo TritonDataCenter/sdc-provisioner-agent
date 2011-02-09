f00011c76869506167dc6b3f23e765e305b56187
Orlando Vazquez <ovazquez@gmail.com>
2011-02-09 00:33:07 -0800

    AGENT-58: Do not attempt to use sysinfo in build

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,7 @@ export AMQP_HOST=localhost
 export AMQP_LOGIN=guest
 export AMQP_PASSWORD=guest
 export SERVER_UUID=my-silly-uuid
+export NO_SYSINFO=1
 ./test-env.sh local/bin/node provisioner-agent.js &
 sleep 10
 ./test-env.sh local/bin/node ./junit-tests.js
