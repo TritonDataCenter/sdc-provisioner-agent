f886b1ccf97eb130e2267315f7605745a0fb6404
Orlando Vazquez <ovazquez@gmail.com>
2011-02-08 22:45:52 -0800

    AGENT-58: Copy provisioner package tarball to web directory

--- bamboo-smartos.sh
+++ bamboo-smartos.sh
@@ -3,3 +3,16 @@
 set -e
 
 gmake npm
+
+NAME=provisioner
+BRANCH=$(git symbolic-ref HEAD | cut -d'/' -f3)
+DESCRIBE=$(git describe)
+PKG=${NAME}-${BRANCH}-${DESCRIBE}.tgz
+PUBLISH_LOCATION=/rpool/data/coal/live_147/agents/provisioner/${BRANCH}/
+
+if [[ `hostname` = 'bh1-autobuild' ]]; then
+  pfexec mkdir -p $PUBLISH_LOCATION
+  pfexec cp provisioner.tgz $PUBLISH_LOCATION
+else
+  echo scp
+fi
