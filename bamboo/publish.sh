0946ec119e4c72ea4a2a77544379d1444d02a89e
Orlando Vazquez <ovazquez@gmail.com>
2011-02-09 01:58:18 -0800

    AGENT-58: Fix a path bug

--- bamboo/publish.sh
+++ bamboo/publish.sh
@@ -2,7 +2,7 @@
 
 if [[ `hostname` = 'bh1-autobuild' ]]; then
   pfexec mkdir -p $PUBLISH_LOCATION
-  pfexec cp $DIRNAME/provisioner.tgz $PUBLISH_LOCATION
+  pfexec cp provisioner.tgz $PUBLISH_LOCATION
 else
   echo scp
 fi
