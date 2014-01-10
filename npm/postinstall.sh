79cd5c122893a0b2c74634a1b044d29b61336d10
Andres Rodriguez <andres@joyent.com>
2014-01-10 15:32:45 -0500

    TOOLS-385: Provisioner logs should be rotated

--- npm/postinstall.sh
+++ npm/postinstall.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace
 DIR=`dirname $0`
+ROOT=$(cd `dirname $0`/.. && pwd)
 
 export PREFIX=$npm_config_prefix
 export ETC_DIR=$npm_config_etc
@@ -13,8 +14,11 @@ subfile () {
   OUT=$2
   sed -e "s#@@PREFIX@@#$PREFIX#g" \
       -e "s/@@VERSION@@/$VERSION/g" \
+      -e "s#@@ROOT@@#$ROOT#g" \
       $IN > $OUT
 }
 
-subfile "$DIR/../smf/manifests/provisioner.xml.in" "$SMF_DIR/provisioner.xml"
+subfile "$ROOT/smf/method/provisioner.in" "$ROOT/smf/method/provisioner"
+subfile "$ROOT/smf/manifests/provisioner.xml.in" "$SMF_DIR/provisioner.xml"
+chmod +x "$ROOT/smf/method/provisioner"
 svccfg import $SMF_DIR/provisioner.xml
